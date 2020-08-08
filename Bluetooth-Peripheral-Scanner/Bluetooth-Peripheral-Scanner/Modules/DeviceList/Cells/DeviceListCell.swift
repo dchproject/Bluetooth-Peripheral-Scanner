cb904d165bf8c0dba4d5d8e3c7ef9379d196495e
Dima Chumakov <dima.chumakovwork@gmail.com>
2020-08-08 18:45:23 +0300

    addedui to DeviceListCell

--- Bluetooth-Peripheral-Scanner/Bluetooth-Peripheral-Scanner/Modules/DeviceList/Cells/DeviceListCell.swift
+++ Bluetooth-Peripheral-Scanner/Bluetooth-Peripheral-Scanner/Modules/DeviceList/Cells/DeviceListCell.swift
@@ -21,7 +21,29 @@ final class DeviceListCell: UICollectionViewCell, CellProtocol {
         
     }
     
+    fileprivate let nameLabel: UILabel = {
+        let label = UILabel.init()
+        label.font = AppStyling.Font.system.font(ofSize: 16)
+        label.textColor = AppStyling.Color.systemBlack.color()        
+        return label
+    }()
+    
     static let height: CGFloat = 64
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupUI()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        setupConstraints()
+    }
     
 }
 
@@ -31,7 +53,45 @@ extension DeviceListCell: ConfigureProtocol {
     typealias T = Model
     
     func configure(type: Model) {
-        
+        self.nameLabel.text = type.peripheral?.name
+    }
+    
+}
+
+// MARK: - Setup UI
+fileprivate extension DeviceListCell {
+    
+    func setupUI() {
+        addNameLabel()
+    }
+    
+}
+
+// MARK: - Add UI
+fileprivate extension DeviceListCell {
+    
+    func addNameLabel() {
+        addSubview(nameLabel)
+    }
+    
+}
+
+// MARK: - Setup Constraints
+fileprivate extension DeviceListCell {
+    
+    func setupConstraints() {
+        makeNameLabelConstraints()
+    }
+    
+}
+
+// MARK: - Make Constraints
+fileprivate extension DeviceListCell {
+    
+    func makeNameLabelConstraints() {
+        nameLabel.snp.makeConstraints { (make) in
+            make.edges.equalTo(self)
+        }
     }
     
 }
